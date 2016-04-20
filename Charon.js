73854b00499d0270b0c395b30109bbf8a7db9bf7
Gary Bell <gary.bell@codebase8.com>
2016-04-20 13:53:05 +0100

    Add the licence into the top of the file

--- Charon.js
+++ Charon.js
@@ -1,7 +1,31 @@
-/**
- * A library for uploading files and sending them to a server without submitting a full page
- * @author Gary Bell
- */
+/******************************************************************************
+ The MIT License (MIT)
+
+ Copyright (c) 2016 Gary Bell
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ ******************************************************************************
+ *
+ * A library for uploading files and sending them to a server without
+ * submitting a full page form page.
+ *
+ *****************************************************************************/
 
 /**
  * Constructor for Charon
@@ -245,7 +269,7 @@ Charon.prototype = {
     , charonProgress: function (progressEvent) {
         // there is no default functionality for this built in
         Charon.prototype.debug('running progress');
-        
+
         // Check if the progress callback is set, and run the function if so
         if (Charon.prototype.callbackIsSet(Charon.prototype.progressCallback)) {
             Charon.prototype.debug('Calling user defined function ' + Charon.prototype.progressCallback);
